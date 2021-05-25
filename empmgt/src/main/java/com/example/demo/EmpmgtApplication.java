a99df32e9056bdece7cf220a5e993f2ce7f6ae6d
DESKTOP-LA6BSBD\Tushar <tusher7575@gmail.com>
2021-05-25 14:26:20 +0600

    first commit

---
+++ empmgt/src/main/java/com/example/demo/EmpmgtApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpmgtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpmgtApplication.class, args);
+	}
+
+}
